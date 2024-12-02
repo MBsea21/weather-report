ddbe39a2ca8da7c28fabec3f1b5880197769371e
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-12-02 15:18:10 -0800

    added temperature and the color/landscape logic

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// "use strict";
+let color = "";
+let landscape = "" ;
+
+////// temp updates
+////// figuring out how to update temperature in html/css styles needed 
+const tempValue = document.getElementById("tempValue");
+let newTemp = 60;
+tempValue.innerText = newTemp;
+///// figuring out how to add up and down arrows next to the temperature gauge
+///////figuring out how to create behaviors and events for up down arrows 
+///declare color and lanscape functions: 
+
+
+
+// const findTempColorandLandscape = (temp) => {
+// if (temp <= 49) {
+//   color = 'teal';
+//   landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+// }
+// if (temp >= 50 & temp <= 59) {
+//   color = 'green';
+//   landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+// }
+// if (temp >= 60 & temp <= 69) {
+//   color = 'yellow';
+//   landscape = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+// }
+// else if (temp >= 70 & temp <= 79) {
+//   color = 'orange';
+//   landscape = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+// }
+// if (temp >= 80){
+//   color = 'red';
+//   landscape = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+// }
+
+// console.log(color, landscape);
+// return {
+//   'color':color, 
+//   'landscape': landscape
+// }
+// };
+
+// // findTempColorandLandscape(temp)
+
