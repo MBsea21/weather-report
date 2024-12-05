7cadbd67d0e656b24e358515b2d83cd3c75b9e44
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-12-04 21:44:58 -0800

    got api call to work and update information on api.html page

--- src/failedTry.js
+++ src/failedTry.js
@@ -90,12 +90,14 @@ const changeSkyValue = () => {
 };
 
 const changeCity = () => {
-  const newCity = documnet.getElementById('cityNameInput')
-  state.currentCity = newCity
+  const newCity = document.getElementById('cityNameInput')
+  state.currentCity = newCity.value
+  getCityCoordandWeatherData(state.currentCity)
+
 }
 
 const resetCity = () => {
-  const cityNameInput = documnet.getElementById('cityNameInput')
+  const cityNameInput = document.getElementById('cityNameInput')
   const cityNameHeader = document.getElementById('cityName')
   state.currentCity = DEFAULT_CITY
   cityNameInput.innerText = state.currentCity
@@ -157,75 +159,87 @@ const openWeatherConditions= {
   Squall: "🌨⛈🌧🌨⛈🌧🌨⛈🌧", 
   Tornado: "🌪⛈⛈🌪⛈⛈🌪⛈⛈", 
   Clear: "         ☀️   🕊       ", 
-  Cloudy: "☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️"
+  Clouds: "☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️"
 };
 
 
 
 
 
-// const getCityCoordandWeatherData = (currentCity) => {
-//   axios
-//   .get(`http://127.0.0.1:5000/location?q=${currentCity}`)
-//   .then( (response) => {
-//     const coordinates = {}
-//     let lat= response.data[0].lat;
-//     let lon= response.data[0].lon;
-//     coordinates['lat'] = lat
-//     coordinates['lon'] = lon
-//     console.log('success in find latitude and longitude!', coordinates)
-//     return getWeatherData(coordinates)
-//   })
-//   .catch((error) => {
-//     console.log(error,'could not find location data')
-//   })
-// };
+const getCityCoordandWeatherData = (currentCity) => {
+  axios
+  .get(`http://127.0.0.1:5000/location?q=${currentCity}`)
+  .then( (response) => {
+    const coordinates = {}
+    let lat= response.data[0].lat;
+    let lon= response.data[0].lon;
+    coordinates['lat'] = lat
+    coordinates['lon'] = lon
+    console.log('success in find latitude and longitude!', coordinates)
+    return getWeatherData(coordinates)
+  })
+  .catch((error) => {
+    console.log(error,'could not find location data')
+  })
+};
 
 
-// const getWeatherData = (coordinates) => {
-//   axios
-//   .get(`http://127.0.0.1:5000/weather?lat=${coordinates.lat}&lon=${coordinates.lon}`)
-//   .then( (response) => {
-//     const cityWeatherData = {}
-//     let weatherData = response.data['weather'];
-//     let currentWeather = weatherData[0].main;
-//     let tempData = response.data['main']
-//     let tempK = tempData.temp
-//     let tempF = (convertTemp(tempK, "K", "F")).toFixed(1);
-//     cityWeatherData['currentWeather']= currentWeather
-//     cityWeatherData['currentTemp'] = tempF
-//     cityWeatherData['currentTempType'] = "F"
-//     console.log('success in finding weather data for city!',cityWeatherData
-//     )
-//     return cityWeatherData
-//   })
-//   .catch((error) => {
-//     console.log(error,'could not get weather data')
-//   });
+const getWeatherData = (coordinates) => {
+  axios
+  .get(`http://127.0.0.1:5000/weather?lat=${coordinates.lat}&lon=${coordinates.lon}`)
+  .then( (response) => {
+    const cityWeatherData = {}
+    let cityName = response.data['name']
+    let weatherData = response.data['weather'];
+    let currentWeather = weatherData[0].main;
+    let tempData = response.data['main']
+    let tempK = tempData.temp
+    let tempF = (convertTemp(tempK, "K", "F")).toFixed(1);
+    state.currentCity = cityName
+    state.currentSky = currentWeather
+    state.currentTemp = tempF
 
-//   }
+    const cityNameContainer = document.getElementById('headerCityName')
+    cityNameContainer.innerText = state.currentCity
+    const tempValueContainer = document.getElementById('tempValue')
+    tempValueContainer.innerText = state.currentTemp
+    changeSkyinWeatherBox(state.currentSky)
+    changeTempColor(state.currentTemp)
+    changeLandscape(state.currentTemp)
+    cityWeatherData['currentWeather']= currentWeather
+    cityWeatherData['currentTemp'] = tempF
+    cityWeatherData['currentTempType'] = "F"
+    console.log('success in finding weather data for city!',cityWeatherData
+    )
+    return cityWeatherData
+  })
+  .catch((error) => {
+    console.log(error,'could not get weather data')
+  });
 
-// const convertTemp = (temp, inputType, outputType) => {
-//   //celcius to farenheit
-//   if (outputType === "C" & inputType === "F") {
-//     return temp*1.8+32;
-//   }
-//   //farenheit to celcius
-//   else if (inputType === "F" & outputType === "C") {
-//     return (temp-32)/1.8;
-//   }
-//   //kelvin to farenheit
-//   else if (inputType === "K" & outputType === "F") {
-//     return (temp*1.8)-459.67;
-//   }
-//   // kelvin to celcius
-//   else if (inputType === "K" & outputType === "C") {
-//     return temp - 273.15;
-//   }
-//   else {
-//     console.log("error");
-//   };
-// };
+  }
+
+const convertTemp = (temp, inputType, outputType) => {
+  //celcius to farenheit
+  if (outputType === "C" & inputType === "F") {
+    return temp*1.8+32;
+  }
+  //farenheit to celcius
+  else if (inputType === "F" & outputType === "C") {
+    return (temp-32)/1.8;
+  }
+  //kelvin to farenheit
+  else if (inputType === "K" & outputType === "F") {
+    return (temp*1.8)-459.67;
+  }
+  // kelvin to celcius
+  else if (inputType === "K" & outputType === "C") {
+    return temp - 273.15;
+  }
+  else {
+    console.log("error");
+  };
+};
 
 document.addEventListener("DOMContentLoaded",registerEventHandlers)
 changeLandscape(state.currentTemp);
